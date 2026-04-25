5c27c4da3a7e1ecc4858ad2b613242c5db83dbd1
soekikwhr-sketch <soekikwhr@gmail.com>
2026-04-26 08:46:57 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'iching-pwa-v1';
+const ASSETS = [
+  './iching_quantum_divination.html',
+  './manifest.json',
+  './icon-192.svg',
+  './icon-512.svg',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
